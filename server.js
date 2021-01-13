83d4d1a8044cbed067b504c2134b7479d801aa46
jre23 <jre.estrada@gmail.com>
2021-01-13 15:07:38 -0800

    created server.js file

---
+++ server.js
@@ -0,0 +1,30 @@
+// require dependencies
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+// configure the PORT 
+const PORT = process.env.PORT || 3000;
+// models
+const db = require("./models");
+// initialize express app
+const app = express();
+// use logging software
+app.use(logger("dev"));
+// set up middleware
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+// serve static content for the app from the public directory
+app.use(express.static("public"));
+// set up connection to mongodb using mongoose
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+});
+// start the server
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
