588fe4f63decc7c378d793f1c0c1f66b4f93053b
jre23 <jre.estrada@gmail.com>
2021-01-13 19:36:19 -0800

    fixed routing issues

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,11 +1,11 @@
-const mongojs = require("mongojs");
+// require dependencies
 const router = require("express").Router();
 const db = require("../models/index");
-
+// route to get all workouts and sort in ascending order. this route is called by api.js getLastWorkout()
 router.get("/api/workouts", (req, res) => {
     db.Workout.find({})
         .sort({
-            day: -1
+            day: 1
         })
         .then(dbWorkouts => {
             res.json(dbWorkouts);
@@ -14,13 +14,11 @@ router.get("/api/workouts", (req, res) => {
             res.status(400).json(err);
         });
 });
-
-router.put("/api/workouts/:id", ({
-    body
-}, res) => {
+// route to add a workout. this route is called by api.js addExercise(data)
+router.put("/api/workouts/:id", (req, res) => {
     console.log("test put api/workouts/:id route");
     // let workoutId = mongojs.ObjectId(req.params.id);
-    db.Workout.insert(body)
+    db.Workout.insert(req.body)
         .then(dbWorkouts => {
             res.json(dbWorkouts);
         })
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,20 +1,21 @@
-// require path for getting the right directory
+// require dependencies
+const router = require("express").Router();
 const path = require("path");
 
 // html routes
-module.exports = app => {
-    // index route loads index.html
-    app.get("/", (req, res) => {
-        res.sendFile(path.join(__dirname, "../public/index.html"));
-    });
+// index route loads index.html
+router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
 
-    // exercise route loads the exercise.html page,
-    app.get("/exercise", (req, res) => {
-        res.sendFile(path.join(__dirname, "../public/exercise.html"));
-    });
+// exercise route loads the exercise.html page,
+router.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
 
-    // stats route loads the stats.html page,
-    app.get("/stats", (req, res) => {
-        res.sendFile(path.join(__dirname, "../public/stats.html"));
-    });
-};
+// stats route loads the stats.html page,
+router.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
+
+module.exports = router;
