38be4118545ad36f2e3d35bea14dd072da8cc911
jre23 <jre.estrada@gmail.com>
2021-01-15 15:12:55 -0800

    cleaned up code in api-routes.js

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -15,10 +15,6 @@ router.get("/api/workouts", (req, res) => {
             day: 1
         })
         .then(dbWorkouts => {
-
-            console.log("=======dbWorkouts result=======");
-            console.log(dbWorkouts);
-
             res.json(dbWorkouts);
         })
         .catch(err => {
@@ -27,13 +23,7 @@ router.get("/api/workouts", (req, res) => {
 });
 // route to add an exercise. this route is called by api.js addExercise(data)
 router.put("/api/workouts/:id", (req, res) => {
-    console.log("test put api/workouts/:id route");
-    console.log("==========req.params.id========");
     let workoutId = req.params.id;
-    console.log(req.url);
-    console.log(workoutId);
-    // console.log("===========req.body============");
-    console.log(req.body);
     // this if statement takes care of a bug when adding a new exercise. bug: the user can click complete after clicking add exercise and it will submit an empty form
     if (req.body.name.trim().length <= 0) {
         console.log("test workoutData =============");
@@ -48,7 +38,6 @@ router.put("/api/workouts/:id", (req, res) => {
                 res.json(dbWorkouts);
             })
             .catch(err => {
-                console.log("=========api/workouts/:id error=======");
                 console.log(err);
                 res.status(400).json(err);
             });
@@ -57,7 +46,6 @@ router.put("/api/workouts/:id", (req, res) => {
 
 // route to add a workout. this route is called by api.js createWorkout(data = {})
 router.post("/api/workouts", (req, res) => {
-    console.log("test post api/workouts route");
     db.Workout.create(req.body)
         .then(dbWorkouts => {
             res.json(dbWorkouts);
@@ -66,7 +54,7 @@ router.post("/api/workouts", (req, res) => {
             res.status(400).json(err);
         });
 });
-// route to get all workouts in a certain range. this route is called by api.js getWorkoutsInRange()
+// route to get all workouts in a certain range (the last 7 workouts in the database). this route is called by api.js getWorkoutsInRange()
 router.get("/api/workouts/range", (req, res) => {
     db.Workout.aggregate([{
                 $sort: {
